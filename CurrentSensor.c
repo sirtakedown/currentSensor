8b13b998737f79047ae754333bb47325808243de
Matthew Herbert <matthew.herbert@umit.maine.edu>
2013-09-28 14:22:28 -0400

    empty project

---
+++ CurrentSensor.c
@@ -0,0 +1,17 @@
+/*
+ * CurrentSensor.c
+ *
+ * Created: 9/28/2013 2:12:45 PM
+ *  Author: Matthew
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
