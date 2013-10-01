7ab672f0edf95775d460b2c8bcef412a592fdad9
Matthew Herbert <matthew.herbert@umit.maine.edu>
2013-10-01 14:36:03 -0400

    added adcinit function, compiles correctly no warnings

--- CurrentSensor.c
+++ CurrentSensor.c
@@ -24,4 +24,9 @@ int main(void)
 
 void adcinit(void){
 	setbit(ADCSRA,ADEN); //enables ADC
+	setbit(ADCSRA,ADFR); //free running conversion
+	ADCSRA = ((ADCSRA & 0b11111000)|0b00000110); //selects div by 64 clock prescaler
+	ADMUX = ((ADMUX &=0b00111111)|0b01000000);   //selects AVcc (~5 volts) as Vref
+	clearbit(ADMUX,ADLAR); //selects right adjust of ADC result
+	ADMUX &= 0b11100000;   //selects single ended conversion of PF0
 }
