c56c5d91da4d2b24ddb71ba08fedfa2e903d75c9
Matthew Herbert <matthew.herbert@umit.maine.edu>
2013-10-01 14:28:13 -0400

    created define statements for setbit,clearbit,and bytevalue

--- CurrentSensor.c
+++ CurrentSensor.c
@@ -8,6 +8,10 @@
 
 #include <avr/io.h>
 
+#define ByteValue(bit) (1<<(bit))  //converts the bit into a byte value
+#define setbit(reg,bit) reg &= ~(ByteValue(bit))  //sets the corresponding bit in register reg
+#define clearbit(reg,bit) reg |= (ByteValue(bit)) //clears the corresponding bit in regieter reg
+
 void adcinit(void);
 
 int main(void)
@@ -16,4 +20,8 @@ int main(void)
     {
         //TODO:: Please write your application code 
     }
+}
+
+void adcinit(void){
+	setbit(ADCSRA,ADEN); //enables ADC
 }
