7a2e85277f68f93eacc1f01da970743164f52c88
Matthew Herbert <matthew.herbert@umit.maine.edu>
2013-10-17 15:06:58 -0400

    empty project

---
+++ Radar.c
@@ -0,0 +1,16 @@
+/*
+ * Radar.c
+ *
+ * Created: 10/17/2013 2:58:14 PM
+ *  Author: Matthew
+ */ 
+
+#define F_CPU 16000000UL        //Says to the compiler which is our clock frequency, permits the delay functions to be very accurate
+#include <avr/io.h>            /General definitions of the registers values
+#include <util/delay.h>            //This is where the delay functions are located
+
+int radar(void){                //The begin of our main function
+	//This is where our code goes
+	
+	return 0;                //As usual all non-void functions must have a return
+}
