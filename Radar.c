d869dfb5dc7bfe2e46eef3d9ec02ae567a2b053f
Matthew Herbert <matthew.herbert@umit.maine.edu>
2013-10-17 15:16:04 -0400

    added prototypes

--- Radar.c
+++ Radar.c
@@ -6,11 +6,16 @@
  */ 
 
 #define F_CPU 16000000UL        //Says to the compiler which is our clock frequency, permits the delay functions to be very accurate
-#include <avr/io.h>            /General definitions of the registers values
+#include <avr/io.h>            //General definitions of the registers values
 #include <util/delay.h>            //This is where the delay functions are located
 
+void USARTinit(void);
+void USARTsend(unsigned char data);
+unsigned char USARTrecieve(void);
+
 int radar(void){                //The begin of our main function
-	//This is where our code goes
-	
+	//Start and setup USART
+	//function to send byte
+	//function to receive byte
 	return 0;                //As usual all non-void functions must have a return
 }
